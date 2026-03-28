00137366b2bff57141e9df5b6593a201c7dfbd01
GO Video <govideo@Mac.localdomain>
2026-03-28 12:14:42 -0600

    v.1.0

--- virtual-velocity/astro.config.mjs
+++ virtual-velocity/astro.config.mjs
@@ -14,7 +14,7 @@ const httpsConfig = () => {
 };
 
 export default defineConfig({
-    build: {
+  build: {
     inlineStylesheets: 'auto', // CSS < 4KB se inlinea automáticamente
   },
   vite: {
@@ -32,7 +32,7 @@ export default defineConfig({
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://unpkg.com",
         "font-src 'self' data: https://fonts.gstatic.com https://cdn.jsdelivr.net https://unpkg.com",
         "img-src 'self' data: https:",
-        "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://t.contentsquare.net https://api.openai.com https://speech.googleapis.com https://translate.google.com https://res.cloudinary.com",
+        "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://t.contentsquare.net https://api.openai.com https://speech.googleapis.com https://translate.google.com https://res.cloudinary.com https://analytics-api-s.cloudinary.com https://unpkg.com",
         "frame-src 'self' https://my.matterport.com",
         "media-src 'self' blob: data: https://res.cloudinary.com",
         "worker-src 'self' blob:",
